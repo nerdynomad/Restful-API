37c0513fc82213879f7e7e1152c4b2659230994e
Abhishek Saw <66946778+nerdynomad@users.noreply.github.com>
2020-08-11 14:11:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,8 @@ const postRoute = require('./routes/posts')
 app.use('/posts', postRoute)
 
 
-//Routes
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
-
-
 //Connect DB
-mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
+mongoose.connect('MongoDB Path', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
     console.log('Connected to DB')
 })
 
